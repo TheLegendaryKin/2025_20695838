7c3b1cf3dee158db5bd5b3f5bb6294dfef501376
Sohaib Akmal <shoaibakmal1@outlook.com>
2026-02-11 10:49:08 +0000

    Worksheet5: add CPack install rules and doxygen-ready header

--- Worksheet5/adder.h
+++ Worksheet5/adder.h
@@ -1,3 +1,4 @@
+// adder.h
 #ifndef MATHSLIBADDERH
 #define MATHSLIBADDERH
 
@@ -7,19 +8,6 @@
 
 
 
-#if defined(_WIN32)
-  #ifdef mathsSTATIC
-    #define MATHSLIBAPI
-  #else
-    #ifdef mathsEXPORTS
-      #define MATHSLIBAPI __declspec(dllexport)
-    #else
-      #define MATHSLIBAPI __declspec(dllimport)
-    #endif
-  #endif
-#else
-  #define MATHSLIBAPI
-#endif
 
 /** Adding function
  * This function adds two numbers.
@@ -27,6 +15,6 @@
  * @param b is the second number
  * @return sum of a and b
  */
-MATHSLIBAPI int add(int a, int b);
+int add(int a, int b);
 
 #endif
